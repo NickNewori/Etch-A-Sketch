cf78f9aba565fd79c051fa194e3966d7daea7d5f
Komi727 <91585872+NickNewori@users.noreply.github.com>
2023-10-14 16:47:09 -0400

    adding events

--- EtchASketch/canvasEvents.js
+++ EtchASketch/canvasEvents.js
@@ -1,7 +1,7 @@
-const container = document.querySelector('#container');
+// const container = document.querySelector('#container');
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = "THis is a sample test!!"
+// const content = document.createElement('div');
+// content.classList.add('content');
+// content.textContent = "THis is a sample test!!"
 
-container.appendChild(content);
+// container.appendChild(content);
