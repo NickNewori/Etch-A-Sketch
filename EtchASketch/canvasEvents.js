2bfb581f48d233259e9189ae65254e6fc42cee59
Komi727 <91585872+NickNewori@users.noreply.github.com>
2023-10-14 18:34:46 -0400

    Added 16x16 grid dom elements

--- EtchASketch/canvasEvents.js
+++ EtchASketch/canvasEvents.js
@@ -1,7 +1,158 @@
-// const container = document.querySelector('#container');
+const container = document.querySelector('#container');
 
-// const content = document.createElement('div');
-// content.classList.add('content');
-// content.textContent = "THis is a sample test!!"
+// Create the 16x16 grid on each line
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container.appendChild(content);
+}
+const container2 = document.querySelector('#container2');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container2.appendChild(content);
+}
+const container3 = document.querySelector('#container3');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container3.appendChild(content);
+}
 
-// container.appendChild(content);
+const container4 = document.querySelector('#container4');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container4.appendChild(content);
+}
+
+const container5 = document.querySelector('#container5');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container5.appendChild(content);
+}
+
+const container6 = document.querySelector('#container6');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container6.appendChild(content);
+}
+
+const container7 = document.querySelector('#container7');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container7.appendChild(content);
+}
+
+const container8 = document.querySelector('#container8');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container8.appendChild(content);
+}
+
+const container9 = document.querySelector('#container9');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container9.appendChild(content);
+}
+
+const container10 = document.querySelector('#container10');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container10.appendChild(content);
+}
+
+const container11 = document.querySelector('#container11');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container11.appendChild(content);
+}
+
+const container12 = document.querySelector('#container12');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container12.appendChild(content);
+}
+
+const container13 = document.querySelector('#container13');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container13.appendChild(content);
+}
+
+const container14 = document.querySelector('#container14');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container14.appendChild(content);
+}
+
+const container15 = document.querySelector('#container15');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container15.appendChild(content);
+}
+const container16 = document.querySelector('#container16');
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.border = '1px solid black';
+    content.style.height = '25px';
+    content.style.width = '25px';
+    container16.appendChild(content);
+}
